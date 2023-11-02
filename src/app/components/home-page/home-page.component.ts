2106bf8faeca244c12ffc9bdd7d57a9719b8ea77
marcoDmc <marcodamasceno07@gmail.com>
2023-11-02 07:25:20 -0300

    style: updated style homepage

--- src/app/components/home-page/home-page.component.ts
+++ src/app/components/home-page/home-page.component.ts
@@ -1,10 +1,19 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-home-page',
   templateUrl: './home-page.component.html',
-  styleUrls: ['./home-page.component.scss']
+  styleUrls: ['./home-page.component.scss'],
 })
 export class HomePageComponent {
+  constructor(private router: Router) {}
 
+  onRedirectLoginPage(): void {
+    this.router.navigate(['signin']);
+  }
+
+  onRedirectDashboard(): void {
+    this.router.navigate(['app']);
+  }
 }
