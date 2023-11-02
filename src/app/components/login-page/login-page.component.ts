58d2a2c1a1fb2e1d1b879364808c2b4b212543b7
marcoDmc <marcodamasceno07@gmail.com>
2023-11-02 07:25:07 -0300

    style: updated style login

--- src/app/components/login-page/login-page.component.ts
+++ src/app/components/login-page/login-page.component.ts
@@ -1,10 +1,42 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { FormBuilder } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login-page',
   templateUrl: './login-page.component.html',
-  styleUrls: ['./login-page.component.scss']
+  styleUrls: ['./login-page.component.scss'],
 })
 export class LoginPageComponent {
+  constructor(private formServiceBuilder: FormBuilder) {}
 
+  protected router = inject(Router);
+
+  protected form = this.formServiceBuilder.group({
+    email: [''],
+    password: [''],
+    checkbox: [false],
+  });
+
+  onLogin(): void {
+    console.log(this.form.value);
+  }
+
+  onRedirectSignup(): void {
+    this.router.navigate(['signup']);
+  }
+
+  onShowHidePassword(show: any, hide: any) {
+    if (show.changeDetector._lView[0].className == 'eye') {
+      show.changeDetector._lView[0].removeAttribute('class');
+      show.changeDetector._lView[0].setAttribute('class', 'eyeSlash');
+      hide.changeDetector._lView[0].removeAttribute('class');
+      hide.changeDetector._lView[0].setAttribute('class', 'eye');
+    } else {
+      hide.changeDetector._lView[0].removeAttribute('class');
+      hide.changeDetector._lView[0].setAttribute('class', 'eyeSlash');
+      show.changeDetector._lView[0].removeAttribute('class');
+      show.changeDetector._lView[0].setAttribute('class', 'eye');
+    }
+  }
 }
