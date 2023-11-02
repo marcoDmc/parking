11908d884f6d5a21af86898105ea3eab27260c6c
marcoDmc <marcodamasceno07@gmail.com>
2023-11-02 02:20:54 -0300

    feat: added component help

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,10 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { SharedRoutingModule } from './shared-routing.module';
+import { ProfileComponent } from './profile/profile.component';
+import { HelpComponent } from './help/help.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    ProfileComponent,
+    HelpComponent
+  ],
   imports: [
     CommonModule,
     SharedRoutingModule
