0dac9dcee48369d0818742fce66224a01bca5e1f
marcoDmc <marcodamasceno07@gmail.com>
2023-11-02 02:21:29 -0300

    feat: added component notfound

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -4,12 +4,14 @@ import { CommonModule } from '@angular/common';
 import { SharedRoutingModule } from './shared-routing.module';
 import { ProfileComponent } from './profile/profile.component';
 import { HelpComponent } from './help/help.component';
+import { NotFoundComponent } from './not-found/not-found.component';
 
 
 @NgModule({
   declarations: [
     ProfileComponent,
-    HelpComponent
+    HelpComponent,
+    NotFoundComponent
   ],
   imports: [
     CommonModule,
