ab1a6ce0f113e319ddf6698dd42a0618105c28ee
marcoDmc <marcodamasceno07@gmail.com>
2023-11-02 07:22:34 -0300

    style: updated style profile

--- src/app/shared/profile/profile.component.ts
+++ src/app/shared/profile/profile.component.ts
@@ -3,8 +3,19 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-profile',
   templateUrl: './profile.component.html',
-  styleUrls: ['./profile.component.scss']
+  styleUrls: ['./profile.component.scss'],
 })
 export class ProfileComponent {
+  constructor() {}
+  openAndClosed(popup: Element): void {
+    console.log(popup.className);
 
+    if (popup.className == 'popup') {
+      popup.removeAttribute('class');
+      popup.setAttribute('class', 'popup_none');
+    } else {
+      popup.removeAttribute('class');
+      popup.setAttribute('class', 'popup');
+    }
+  }
 }
