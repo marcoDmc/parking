4a351bd71a1e36c2c576d02ebe2e7b50959dd9b2
marcoDmc <marcodamasceno07@gmail.com>
2023-11-02 02:22:10 -0300

    feat: added component notification

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -5,13 +5,15 @@ import { SharedRoutingModule } from './shared-routing.module';
 import { ProfileComponent } from './profile/profile.component';
 import { HelpComponent } from './help/help.component';
 import { NotFoundComponent } from './not-found/not-found.component';
+import { NotificationComponent } from './notification/notification.component';
 
 
 @NgModule({
   declarations: [
     ProfileComponent,
     HelpComponent,
-    NotFoundComponent
+    NotFoundComponent,
+    NotificationComponent
   ],
   imports: [
     CommonModule,
