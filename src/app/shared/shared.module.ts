c931af82d4d90bc12e5148a0d0673b0065be8e78
marcoDmc <marcodamasceno07@gmail.com>
2023-11-06 15:38:24 -0300

    chore: import a new icon

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,17 +1,20 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
+import { NgIconsModule } from '@ng-icons/core';
 import { SharedRoutingModule } from './shared-routing.module';
 import { NotFoundComponent } from './not-found/not-found.component';
 
+import { heroEyeSolid, heroEyeSlashSolid } from '@ng-icons/heroicons/solid';
 
 @NgModule({
-  declarations: [
-    NotFoundComponent
-  ],
+  declarations: [NotFoundComponent],
   imports: [
     CommonModule,
-    SharedRoutingModule
-  ]
+    SharedRoutingModule,
+    NgIconsModule.withIcons({
+      heroEyeSolid,
+      heroEyeSlashSolid,
+    }),
+  ],
 })
-export class SharedModule { }
+export class SharedModule {}
