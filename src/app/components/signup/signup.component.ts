2b9161de6c7428d37d8abe233b9f9c5b6577d050
marcoDmc <marcodamasceno07@gmail.com>
2023-11-07 17:33:07 -0300

    refactor: changed document. queryselector for viewchild

--- src/app/components/signup/signup.component.ts
+++ src/app/components/signup/signup.component.ts
@@ -1,4 +1,4 @@
-import { Component, inject } from '@angular/core';
+import { Component, ViewChild, inject } from '@angular/core';
 import { FormBuilder } from '@angular/forms';
 import { Router } from '@angular/router';
 
@@ -8,6 +8,12 @@ import { Router } from '@angular/router';
   styleUrls: ['./signup.component.scss'],
 })
 export class SignupComponent {
+  @ViewChild('eye') show!: any;
+  @ViewChild('eyeSlash') hide!: any;
+
+  @ViewChild('eyes') shows!: any;
+  @ViewChild('eyesSlash') hides!: any;
+
   constructor(private formServiveBuilder: FormBuilder) {}
   protected router = inject(Router);
 
@@ -27,31 +33,31 @@ export class SignupComponent {
 
   onlogin = () => this.router.navigate(['signin']);
 
-  onShowHideLastPassword(show: any, hide: any) {
-    if (show.changeDetector._lView[0].className == 'eye') {
-      show.changeDetector._lView[0].removeAttribute('class');
-      show.changeDetector._lView[0].setAttribute('class', 'eyeSlash');
-      hide.changeDetector._lView[0].removeAttribute('class');
-      hide.changeDetector._lView[0].setAttribute('class', 'eye');
+  onShowHideLastPassword() {
+    if (this.show.changeDetector._lView[0].className == 'eye') {
+      this.show.changeDetector._lView[0].removeAttribute('class');
+      this.show.changeDetector._lView[0].setAttribute('class', 'eyeSlash');
+      this.hide.changeDetector._lView[0].removeAttribute('class');
+      this.hide.changeDetector._lView[0].setAttribute('class', 'eye');
     } else {
-      hide.changeDetector._lView[0].removeAttribute('class');
-      hide.changeDetector._lView[0].setAttribute('class', 'eyeSlash');
-      show.changeDetector._lView[0].removeAttribute('class');
-      show.changeDetector._lView[0].setAttribute('class', 'eye');
+      this.hide.changeDetector._lView[0].removeAttribute('class');
+      this.hide.changeDetector._lView[0].setAttribute('class', 'eyeSlash');
+      this.show.changeDetector._lView[0].removeAttribute('class');
+      this.show.changeDetector._lView[0].setAttribute('class', 'eye');
     }
   }
 
-  onShowHideNewPassword(show: any, hide: any) {
-    if (show.changeDetector._lView[0].className == 'eye') {
-      show.changeDetector._lView[0].removeAttribute('class');
-      show.changeDetector._lView[0].setAttribute('class', 'eyeSlash');
-      hide.changeDetector._lView[0].removeAttribute('class');
-      hide.changeDetector._lView[0].setAttribute('class', 'eye');
+  onShowHideNewPassword() {
+    if (this.shows.changeDetector._lView[0].className == 'eye') {
+      this.shows.changeDetector._lView[0].removeAttribute('class');
+      this.shows.changeDetector._lView[0].setAttribute('class', 'eyeSlash');
+      this.hides.changeDetector._lView[0].removeAttribute('class');
+      this.hides.changeDetector._lView[0].setAttribute('class', 'eye');
     } else {
-      hide.changeDetector._lView[0].removeAttribute('class');
-      hide.changeDetector._lView[0].setAttribute('class', 'eyeSlash');
-      show.changeDetector._lView[0].removeAttribute('class');
-      show.changeDetector._lView[0].setAttribute('class', 'eye');
+      this.hides.changeDetector._lView[0].removeAttribute('class');
+      this.hides.changeDetector._lView[0].setAttribute('class', 'eyeSlash');
+      this.shows.changeDetector._lView[0].removeAttribute('class');
+      this.shows.changeDetector._lView[0].setAttribute('class', 'eye');
     }
   }
 }
