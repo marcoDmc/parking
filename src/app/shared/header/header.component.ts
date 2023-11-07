5e034e6e308b11e4a74d4b8ca8df161df4942c97
marcoDmc <marcodamasceno07@gmail.com>
2023-11-07 07:24:02 -0300

    feat: create component header

---
+++ src/app/shared/header/header.component.ts
@@ -0,0 +1,27 @@
+import { Component, EventEmitter, OnInit, Output, inject } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.scss'],
+})
+export class HeaderComponent implements OnInit {
+  protected router = inject(Router);
+
+  @Output() Login: EventEmitter<any> = new EventEmitter();
+
+  onRedirectLoginPage(): void {
+    this.router.navigate(['signin']);
+  }
+
+  onRedirectDashboard(): void {
+    this.router.navigate(['app']);
+  }
+  onRedirectAbout(): void {
+    this.router.navigate(['about']);
+  }
+  ngOnInit(): void {
+    this.Login.emit(this.onRedirectLoginPage);
+  }
+}
