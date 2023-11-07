a112b09692520375f5fab8766e5dcd7fe617f6ec
marcoDmc <marcodamasceno07@gmail.com>
2023-11-07 07:29:08 -0300

    style: removed blank space

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -3,7 +3,6 @@ import { CommonModule } from '@angular/common';
 import { NgIconsModule } from '@ng-icons/core';
 import { SharedRoutingModule } from './shared-routing.module';
 import { NotFoundComponent } from './not-found/not-found.component';
-
 import { heroEyeSolid, heroEyeSlashSolid } from '@ng-icons/heroicons/solid';
 
 @NgModule({
