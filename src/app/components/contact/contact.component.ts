b2efb9d8e53c8b8855436ff18fe57f45412588ad
marcoDmc <marcodamasceno07@gmail.com>
2023-11-07 17:30:51 -0300

    feat: create component contact

--- src/app/components/contact/contact.component.ts
+++ src/app/components/contact/contact.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-contact',
   templateUrl: './contact.component.html',
-  styleUrls: ['./contact.component.scss']
+  styleUrls: ['./contact.component.scss'],
 })
 export class ContactComponent {
+  private formServiceBuilder = inject(FormBuilder);
 
+  name: string = 'send';
+  className: string = 'button1';
+
+  protected form = this.formServiceBuilder.group({
+    firstName: [''],
+    lastName: [''],
+    message: [''],
+    email: [''],
+  });
 }
