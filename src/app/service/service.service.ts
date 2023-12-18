122d3f2aaf26eea307f7579617761d665d7dfa16
marcoDmc <marcodamasceno07@gmail.com>
2023-12-18 17:57:13 -0300

    build: added service

---
+++ src/app/service/service.service.ts
@@ -0,0 +1,45 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { environment } from '../environment/environment';
+import { ISignup } from '../interfaces/ISignup';
+import { ISignin } from '../interfaces/ISignin';
+import { IUpdatePhoto } from '../interfaces/IUpdatePhoto';
+import { IUpdatePasswordUser } from '../interfaces/IUpdatePasswordUser';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class ServiceService {
+  constructor(private Http: HttpClient) {}
+
+  OnLogin(email: string, password: string): Observable<ISignin> {
+    return this.Http.post<ISignin>(`${environment.url}/login/user`, {
+      email,
+      password,
+    });
+  }
+
+  OnSignup(body: object): Observable<ISignup> {
+    return this.Http.post<ISignup>(`${environment.url}/create/user`, body);
+  }
+
+  OnUpdate(body: object): Observable<IUpdatePasswordUser> {
+    return this.Http.patch<IUpdatePasswordUser>(
+      `${environment.url}/update/user`,
+      body
+    );
+  }
+
+  OnUpdatePhotoUser(formdata: FormData): Observable<IUpdatePhoto> {
+    return this.Http.patch<IUpdatePhoto>(
+      `${environment.url}/update/photo`,
+      formdata
+    );
+  }
+  OnDownloadPhoto(body: object): Observable<any> {
+    return this.Http.post(`${environment.url}/download/profile`, body, {
+      responseType: 'blob',
+    });
+  }
+}
