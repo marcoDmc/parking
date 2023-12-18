977a2cc9b67b892811d960a92e24f24c0acddac4
marcoDmc <marcodamasceno07@gmail.com>
2023-12-18 17:55:10 -0300

    chore: added guard

---
+++ src/app/Guard/guard.guard.ts
@@ -0,0 +1,7 @@
+import { CanActivateFn } from '@angular/router';
+import { methods } from '../utils/Func';
+
+export const guardGuard: CanActivateFn = (route, state) => {
+  const result = methods.VerifyToken();
+  return true;
+};
