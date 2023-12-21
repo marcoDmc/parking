6b68731eb33e617c13246302b89e7be58bd566ca
marcoDmc <marcodamasceno07@gmail.com>
2023-12-21 18:36:51 -0300

    chore: added mock file to simulate data returned from database

---
+++ src/app/mock/testimonials.ts
@@ -0,0 +1,33 @@
+export const person = [
+  {
+    name: 'Isabella Cooper',
+    pharse:
+      'After I started using the parking application, my life became much easier. The application automatically records all cars entering and leaving, and I can view the information in real time. This allows me to make faster and more efficient parking decisions.',
+    url: 'https://www.shutterstock.com/image-photo/studio-portrait-happy-woman-long-260nw-1954427407.jpg',
+  },
+
+  {
+    name: 'Charlotte Carter',
+    pharse:
+      'After I started using the parking application, my life became much easier. The application shows me the available parking spaces in real time, which allows me to find a space quickly and without wasting time.',
+    url: 'https://ims.georgetown.edu/wp-content/uploads/sites/295/2023/04/Dominic_profile_2023-962x1024.png',
+  },
+  {
+    name: 'Oliver Williams',
+    pharse:
+      'After I started using the parking application, my life became much easier. The application provides me with all the information I need about parking, and I can access this information anytime, anywhere.',
+    url: '../../assets/man.jpg',
+  },
+  {
+    name: 'Isabella Cooper',
+    pharse:
+      'After I started using the parking application, my life became much easier. The application automatically records all cars entering and leaving, and I can view the information in real time. This allows me to make faster and more efficient parking decisions.',
+    url: '../../assets/woman1.jpg',
+  },
+  {
+    name: 'Charlotte Carter',
+    pharse:
+      'After I started using the parking application, my life became much easier. The application shows me the available parking spaces in real time, which allows me to find a space quickly and without wasting time.',
+    url: '../../assets/woman2.jpg',
+  },
+];
